621f58d3f97e98f43abde607a8c0e6c53fc4f1ca
Pavankalyan <pavankalyanmagam@gmail.com>
2024-08-05 08:59:12 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Header from "./components/Header.jsx";
+
+import "./App.css";
+import Balance from "./components/Balance.jsx";
+import IncomeExpenses from "./components/IncomeExpenses.jsx";
+import TransactionList from "./components/TransactionList.jsx";
+import AddTransaction from "./components/AddTransaction.jsx";
+import { GlobalProvider } from "./context/GlobalState.jsx";
+
+const App = () => {
+  return (
+    <>
+      <GlobalProvider>
+        <Header />
+        <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
+    </>
+  );
+};
+
+export default App;
